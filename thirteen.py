c9b17fa7b073b90e02d7baf7224d8c9c975923a5
haga yuya <hagayuya@haganoMacBook-ea.local>
2020-08-08 11:34:13 +0900

    moving

--- thirteen.py
+++ thirteen.py
@@ -3,6 +3,7 @@ from selenium.webdriver.support.events import EventFiringWebDriver, AbstractEven
 
 from selenium.webdriver.common.keys import Keys
 
+from selenium.webdriver import ActionChains
 
 import time
 
@@ -15,17 +16,31 @@ class MyListener(AbstractEventListener):
 driver = Firefox()
 ef_driver = EventFiringWebDriver(driver, MyListener())
 # ef_driver.get("https://hello-flask0412.herokuapp.com")
-ef_driver.get("https://www.doctorwho.tv/games/thirteen#_")
+#ef_driver.get("https://www.doctorwho.tv/games/thirteen#_")
+ef_driver.get("https://asset-manager.bbcchannels.com/m/2fzi3/")
 
 # element = ef_driver.find_element_by_id("page-title")
-time.sleep(1)
-element = ef_driver.find_element_by_xpath('/html/body/div/div[1]/div[2]') 
-print(element)
+#element = ef_driver.find_element_by_xpath('/html/body/div/div[1]/div[2]') 
+
+element = ef_driver.find_element_by_class_name('best-container')
+#element = ef_driver.find_element_by_xpath('//*[@id="full-width"]/div[1]/div/iframe/html/body/div/div[1]/div[2]/div[1]') 
+l = (element.location)
 #element.send_keys("selenium")
 #element.send_keys(Keys.ENTER)
 
+actions = ActionChains(ef_driver)
+actions.move_by_offset(l['x'],l['y'])
+for i in range(0,100):
+	actions.click()
+	actions.key_down(Keys.LEFT)
+	actions.key_down(Keys.DOWN)
+	actions.key_down(Keys.RIGHT)
+	actions.key_down(Keys.DOWN)
 
+	actions.perform()
+	time.sleep(0.1)
+	actions = ActionChains(ef_driver)
 
-# time.sleep(5)
-# # ブラウザを終了する。
-# ef_driver.close()
+time.sleep(2)
+# ブラウザを終了する。
+#ef_driver.close()
