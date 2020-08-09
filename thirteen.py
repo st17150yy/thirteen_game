126229554e6412b7b078d66c668320659422a7b1
st17150yy <st17150yy@gm.ibaraki-ct.ac.jp>
2020-08-10 01:26:10 +0900

    配列作成

--- thirteen.py
+++ thirteen.py
@@ -35,45 +35,82 @@ actions = ActionChains(ef_driver)
 
 t = 0
 init_score = getScore(ef_driver)
+past_init_score = init_score
 score = init_score
+score_changed = True
 
-for i in range(0,200):
+for i in range(0,20):
 #	actions.click()
 
+	init_score = getScore(ef_driver)
 	actions.key_down(Keys.LEFT)
 	actions.pause(t)
-	score += getScore(ef_driver)
+
 
 	actions.key_down(Keys.DOWN)
 	actions.pause(t)
-	score += getScore(ef_driver)
-
-
+	"""
 	actions.key_down(Keys.RIGHT)
 	actions.pause(t)
-	score += getScore(ef_driver)
+
 
 	actions.key_down(Keys.DOWN)
 	actions.pause(t)
-	score += getScore(ef_driver)
-
+	"""
 
 	time.sleep(0)
-	if score == init_score:
-		actions.key_down(Keys.UP)
+	if getScore(ef_driver) == init_score:
+		actions.key_down(Keys.RIGHT)
+		actions.key_down(Keys.DOWN)
+
 		actions.pause(t)
-		print("UP")
-	init_score = getScore(ef_driver)
-	score = init_score
+#		print("RIGHT!!")
+
+		if past_init_score == init_score:
+			ActionChains(ef_driver).key_down(Keys.UP).perform()
+#			print("UP!!")
+		past_init_score = init_score
+
 	actions.perform()
+
+
+
+	init_score = getScore(ef_driver)
 	actions = ActionChains(ef_driver)
 
+
+element  = ef_driver.find_element_by_class_name('tile-container' )
+
+table = element.get_attribute('innerHTML')
+table = table.replace('<div class="tile-inner"></div></div>','\n')
+table = table.replace('<div class="tile tile-','')
+table = table.replace('tile-position-',',(')
+table = table.replace('-',',')
+table = table.replace('">',')')
+table = table.replace('tile','')
+table = table.replace(' ,new','')
+table = table.replace(' ,merged','')
+table_list = table.split('\n')
+
+# print('merged : ',NbMerged)
+length = len(table_list)
+for i in range(len(table_list)):
+    if i+1 < length :
+        if table_list[i] == table_list[i+1]:
+            table_list.pop(i)
+            table_list.pop(i)
+            length -= 2
+
+
+
+print(table)
+print(table_list)
+print('row table\n',element.get_attribute('innerHTML'))
 element = ef_driver.find_element_by_class_name('best-container')
 l = element.location
 print(element.text)
 
-time.sleep(2)
+#time.sleep(2)
 # ブラウザを終了する。
-#ef_driver.close()
-
-
+ef_driver.close()
+# driver.close()
