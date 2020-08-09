3680a23bc3c8b2cdaac3a09ab2bc4f23a61ca0e6
st17150yy <st17150yy@gm.ibaraki-ct.ac.jp>
2020-08-09 23:56:12 +0900

    add ml solve

--- thirteen.py
+++ thirteen.py
@@ -17,50 +17,63 @@ def merged(navigated_driver,merged_query='tile-merged'):
 	elements = navigated_driver.find_elements_by_class_name(merged_query)
 	return len(elements) != 0;
 
-driver = webdriver.Firefox()
+def getScore(navigated_driver,getScore_query='score-container'):
+	element = navigated_driver.find_element_by_class_name(getScore_query)
+	return (eval(element.text.replace('\n','')));
+
+
+
+driver = webdriver.Chrome()
 ef_driver = EventFiringWebDriver(driver, MyListener())
 ef_driver.get('https://asset-manager.bbcchannels.com/m/2fzi3/')
 
 
 element = ef_driver.find_element_by_class_name('best-container')
 l = element.location
-
+print(element.text)
 actions = ActionChains(ef_driver)
 actions.move_by_offset(l['x'],l['y'])
 
-time.sleep(5)
+#time.sleep(5)
 
 t = 0
-noMerge = False
+init_score = getScore(ef_driver)
+score = init_score
 
-for i in range(0,200):
-	actions.click()
+for i in range(0,10):
+#	actions.click()
 
 	actions.key_down(Keys.LEFT)
 	actions.pause(t)
-	noMerge = not merged(ef_driver) and noMerge
-	
+	score += getScore(ef_driver)
+
 	actions.key_down(Keys.DOWN)
 	actions.pause(t)
-	noMerge = not merged(ef_driver) and noMerge
+	score += getScore(ef_driver)
+
 
 	actions.key_down(Keys.RIGHT)
 	actions.pause(t)
-	noMerge = not merged(ef_driver) and noMerge
+	score += getScore(ef_driver)
 
 	actions.key_down(Keys.DOWN)
 	actions.pause(t)
-	noMerge = not merged(ef_driver) and noMerge
+	score += getScore(ef_driver)
+
 
 	time.sleep(0)
-	
-	if noMerge:
-#		actions.key_down(Keys.UP)
+	if score == init_score:
+		actions.key_down(Keys.UP)
 		actions.pause(t)
 		print("UP")
-	
+	init_score = getScore(ef_driver)
+	score = init_score
 	actions.perform()
 	actions = ActionChains(ef_driver)
+
+element = ef_driver.find_element_by_class_name('best-container')
+l = element.location
+print(element.text)
 
 time.sleep(2)
 # ブラウザを終了する。
